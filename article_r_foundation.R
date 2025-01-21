39df293b272673300a85be6f118842dd812bd903
Shi Varong <shivarong@gmail.com>
2025-01-21 23:42:25 +0700

    Delete extra lines between variables and print()

--- article_r_foundation.R
+++ article_r_foundation.R
@@ -7,37 +7,31 @@ print(x)
 
 ## numeric
 age <- 10
-
 print(age)
 
 
 ## character
 name <- "Ben Tennyson"
-
 print(name)
 
 
 ## logical
 is_hero <- TRUE
-
 print(is_hero)
 
 
 ## date
 date_of_birth <- as.Date("1995-12-27")
-
 print(date_of_birth)
 
 
 ## factor
 gender <- as.factor("Male")
-
 print(gender)
 
 
 ## check class
 class(age)
-
 
 
 # ------------------------------
@@ -47,19 +41,16 @@ class(age)
 
 ## vector
 v = c(1, 3, 5, 7, 9)
-
 print(v)
 
 
 ## matrix
 m <- matrix(1:9, ncol = 3)
-
 print(m)
 
 
 ## array
 a <- array(1:24, dim = c(4, 3, 2))
-
 print(a)
 
 
@@ -67,9 +58,9 @@ print(a)
 grocery_list = list("apple",
                     "milk",
                     TRUE,
-                    250, c(1, 3, 5, 7, 9),
+                    250,
+                    c(1, 3, 5, 7, 9),
                     list("Walmart", "Target"))
-
 print(grocery_list)
 
 
@@ -80,7 +71,6 @@ groceries <- data.frame(
   Quantity = c(5, 2, 1),
   Price = c(1.50, 0.75, 2.50)
 )
-
 print(groceries)
 
 
@@ -120,5 +110,4 @@ print("Hello, World!")
 greeting <- function(name) {
   print(paste("Hello", name))
 }
-
 greeting("John")
