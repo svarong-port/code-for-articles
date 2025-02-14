2a935369b4626e6544d290eb73f7744284b6e936
Shinin Varongchayakul <shivarong@gmail.com>
2025-02-15 00:43:39 +0700

    Initial commit

---
+++ article_data_viz_in_r/article_ggplot2.R
@@ -0,0 +1,102 @@
+# Code for Data Viz in R With ggplot2
+
+
+# 1. Install ggplot2
+install.packages("ggplot2")
+
+# 2. Load ggplot2
+library(ggplot2)
+
+# 3. Load the dataset
+install.packages("palmerpenguins")
+library(palmerpenguins)
+
+head(penguins)
+?penguins
+
+
+
+# ----------------------------------
+
+
+
+# 4. Basic ggplot2
+
+## 4.1 Basic graph
+ggplot(penguins, aes(x = body_mass_g,
+                     y = flipper_length_mm)) +
+  geom_point()
+
+## 4.2 Adding a variable
+ggplot(penguins, aes(x = body_mass_g,
+                     y = flipper_length_mm,
+                     color = species)) +
+  geom_point()
+
+## 4.3 Defining an attribute
+ggplot(penguins, aes(x = body_mass_g,
+                     y = flipper_length_mm,
+                     color = species)) +
+  geom_point(shape = 22)
+
+
+
+# ----------------------------------
+
+
+
+# 5. More Customisations
+
+## 5.1 Theme
+ggplot(penguins, aes(x = body_mass_g,
+                     y = flipper_length_mm,
+                     color = species)) +
+  geom_point() +
+  theme_classic()
+
+
+## 5.2 Text
+ggplot(penguins, aes(x = body_mass_g,
+                       y = flipper_length_mm,
+                       color = species)) +
+  geom_point() +
+  theme_classic() +
+  
+  # Adjust text size
+  theme(plot.title = element_text(size = 16, face = "bold"),
+        axis.title.x = element_text(size = 14),
+        axis.title.y = element_text(size = 14),
+        legend.title = element_text(size = 14),
+        legend.text = element_text(size = 12),
+        strip.text = element_text(size = 14, face = "bold")) +
+    
+    # Add a title, labels, and a legend
+    labs(title = "Penguin Body Mass vs. Flipper Length",
+         x = "Body Mass (g)",
+         y = "Flipper Length (mm)",
+         color = "Penguin Species") +
+    
+    # Add annotation to each facet
+    annotate("text", x = 3000, y = 225,
+             label = "Larger penguins tend to \n have longer flippers",
+             size = 5,
+             color = "gray",
+             hjust = 0)
+  
+## 5.3 Facet
+
+### 5.3.1 facet_wrap()
+ggplot(penguins, aes(x = body_mass_g,
+                     y = flipper_length_mm,
+                     color = species)) +
+  geom_point() +
+  theme_bw() +
+  facet_wrap(~species)
+
+### 5.3.2 facet_grid()
+ggplot(penguins, aes(x = body_mass_g,
+                     y = flipper_length_mm,
+                     color = species)) +
+  geom_point() +
+  theme_bw() +
+  facet_grid(sex~species)
