9da5fdcf2b4a743532dd29883331f876af1e6464
Shinin Varongchayakul <shivarong@gmail.com>
2025-03-14 22:39:00 +0700

    Initial commit

---
+++ article_km_with_kmeans_in_r.R
@@ -0,0 +1,95 @@
+# Code for k-Means Model in R with kmeans()
+
+
+# Step 1. Load the dataset
+
+## Load
+data(rock)
+
+## Preview the dataset
+head(rock)
+
+## View the structure of the dataset
+str(rock)
+
+
+# ---------------------------------------------
+
+
+# Step 2. Normalise the data
+
+## Scale
+rock_scaled <- scale(rock)
+
+## Check the results
+summary(rock_scaled)
+
+
+# ---------------------------------------------
+
+
+# Step 3. Find the optimal k
+
+## Initialise a vector for within cluster sum of squares (wss)
+wss <- numeric(20)
+
+## For-loop through the wss
+for (k in 1:20) {
+  
+  ## Try the k
+  km <- kmeans(rock_scaled,
+               centers = k,
+               nstart = 20)
+  
+  ## Get WSS for the k
+  wss[k] <- km$tot.withinss
+}
+
+## Plot the wss
+plot(1:20,
+     wss,
+     type = "b",
+     main = "The Number of Clusters vs WSS",
+     xlab = "Number of Clusters",
+     ylab = "WSS")
+
+## optiomal k = 5
+k <- 5
+
+
+# ---------------------------------------------
+
+
+# Step 4. Train the model
+
+## Set see for reproducibility
+set.seed(500)
+
+## Train the model
+km <- kmeans(rock_scaled,
+             centers = k,
+             nstart = 20)
+
+## Print the model
+print(km)
+
+
+# ---------------------------------------------
+
+
+# Step 5. Visualise the clusters
+
+## Create a plot
+plot(rock_scaled[, c("shape", "perm")], 
+     col = km$cluster,
+     pch = 19,
+     main = "K-Means Clustering (Rock Dataset)",
+     xlab = "Shape",
+     ylab = "Permeability")
+
+## Add cluster centers
+points(km$centers[, c("shape", "perm")], 
+       col = 1:3,
+       pch = 4,
+       cex = 2,
+       lwd = 2)
