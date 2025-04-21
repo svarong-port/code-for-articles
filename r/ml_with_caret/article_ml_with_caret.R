2432b254ea6b6741d78cbf6c7e24c6cc50cef211
Shinin Varongchayakul <shivarong@gmail.com>
2025-04-21 20:55:20 +0700

    Complete the code the article

--- r/ml_with_caret/article_ml_with_caret.R
+++ r/ml_with_caret/article_ml_with_caret.R
@@ -1,11 +1,13 @@
 # Code for Machine Learning With caret
 
 
-# Install and load the pacakges
-install.packages("caret") # for machine learning
-install.packages("MASS") # for dataset
+## Install and load packages
 
-# Load
+## Install
+install.packages("caret")
+install.packages("MASS")
+
+## Load
 library(caret)
 library(MASS)
 
@@ -18,99 +20,150 @@ library(MASS)
 ## Load
 data(Boston)
 
-## Create a copy of Boston
-bt <- Boston
+## Preview
+head(Boston)
 
 ## View the structure
+str(Boston)
+
+
+# ---------------------------------------
+
+
+# Prepare the data for regression task
+
+## Create a new dataset
+bt <- Boston
+
+## Convert `chas` to factor
+bt$chas <- factor(bt$chas,
+                  levels = c(1, 0),
+                  labels = c("tract bounds river", "otherwise"))
+
+## Check the result
 str(bt)
 
 
 # ---------------------------------------
 
 
-# Preprocessing
+## Pre-processing
 
-## Dummy encoding
+## Remove near-zero variance (NZV) variables
+nzv_vars <- nearZeroVar(bt[, -14])
 
-### Convert `chas` to factor
-bt$chas <- factor(bt$chas,
-                  levels = c(1, 0),
-                  labels = c("river_bound", "otherwise"))
+## Print nzv_vars
+if (length(nvz_vars) > 0) {
+  
+  ## Remove NZV variables
+  bt <- bt[, !(names(bt) %in% nvz_vars)]
+  
+  ## Print a confirmation message
+  print("Near-zero variance variables removed.")
+} else {
+  ## Print a help text
+  print("No near-zero variance variables found.")
+}
 
-### Prepare a dummy encoding recipe
-dummy_rec <- dummyVars(medv ~ .,
-                       data = bt)
-
-### Apply the recipe
-bt_prep <- predict(dummy_rec,
-                   newdata = bt) |> as.data.frame()
-
-
-### Add `medv` back into the data frame
-bt_prep$medv <- bt$medv
-
-### Check the results
-str(bt_prep)
+## Check the results
+str(bt)
 
 
 # ---------------------------------------
 
 
-# Split the data
+## Split the data
 
-## Create training set index
-train_index <- createDataPartition(bt_prep$medv,
+## Set seed for reproducibility
+set.seed(2025)
+
+## Get training rows
+train_index <- createDataPartition(bt$medv,
                                    p = 0.8,
                                    list = FALSE)
 
-## Create training set
-bt_train <- bt_prep[train_index, ]
+## Create a training set
+bt_train <- bt[train_index, ]
 
-## Create test set
-bt_test <- bt_prep[-train_index, ]
+## Create a test set
+bt_test  <- bt[-train_index, ]
 
 
 # ---------------------------------------
-
-
-# Tune and train the model
-
-## Set up cross-validation
-
-### Set seed for reproducibility
-set.seed(2025)
-
-### Set cross-validation to 10 folds
-train_control <- trainControl(method = "cv",
-                              number = 10,
-                              verboseIter = TRUE)
-
-### Set tune grid
-tune_grid <- expand.grid(mtry = c(2, 4, 6, 8, 10),  
-                         min.node.size = c(1, 5, 10),
-                         splitrule = c("variance"))
 
 
 ## Train a random forest
-rf_model <- train(medv ~ .,
+
+## Set seed for reproducibility
+set.seed(2025)
+
+## Train
+dt_model <- train(medv ~ .,
                   data = bt_train,
                   method = "ranger",
-                  trControl = train_control,
-                  tuneGrid = tune_grid)
+                  trControl = trainControl(method = "cv",
+                                           number = 5,
+                                           verboseIter = TRUE))
 
 
 # ---------------------------------------
 
 
-# Evaluate the model
+## Evaluate the model
 
-## Make predictions
-pred <- predict(rf_model,
-                newdata = bt_test)
+## Predict on test data
+predictions <- predict(dt_model,
+                       newdata = bt_test)
 
-## Compute the performance metrics
-metrics <- postResample(pred = pred,
-                        obs = bt_test$medv)
+## Evaluate performance
+init_mod_results <- postResample(pred = predictions,
+                                 obs = bt_test$medv)
 
 ## Print the results
-metrics
+init_mod_results
+
+
+# ---------------------------------------
+
+
+## Tune the model
+
+## Define tune grid
+tune_grid <- expand.grid(mtry = c(4, 6, 8, 10),
+                         min.node.size = c(3, 5, 7),
+                         splitrule = c("variance", "extratrees"))
+
+## Set seed for reproducibility
+set.seed(2025)
+
+## Train
+dt_model_tuned <- train(medv ~ .,
+                        data = bt_train,
+                        method = "ranger",
+                        trControl = trainControl(method = "cv",
+                                                 number = 5,
+                                                 verboseIter = TRUE),
+                        tuneGrid = tune_grid,
+                        numtree = 1000)
+
+
+# ---------------------------------------
+
+
+## Evaluate the tuned model
+
+## Predict on test data
+pred_new <- predict(dt_model_tuned,
+                    newdata = bt_test)
+
+## Evaluate performance
+tuned_mod_results <- postResample(pred = pred_new,
+                                  obs = bt_test$medv)
+
+## Print the results
+tuned_mod_results
+
+## Compare the models
+bind_rows(initial = init_mod_results,
+          tuned = tuned_mod_results,
+          .id = "Model")
