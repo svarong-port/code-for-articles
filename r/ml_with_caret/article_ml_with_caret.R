f0a19e8ffa739d316ab93df5b8c1f10b350345a7
Shinin Varongchayakul <shivarong@gmail.com>
2025-05-03 01:00:04 +0700

    Revise code to build a knn model

--- r/ml_with_caret/article_ml_with_caret.R
+++ r/ml_with_caret/article_ml_with_caret.R
@@ -1,15 +1,14 @@
 # Code for Machine Learning With caret
 
-
 ## Install and load packages
 
 ## Install
 install.packages("caret")
-install.packages("MASS")
+install.packages("mlbench")
 
 ## Load
 library(caret)
-library(MASS)
+library(mlbench)
 
 
 # ---------------------------------------
@@ -18,155 +17,121 @@ library(MASS)
 # Load the dataset
 
 ## Load
-data(Boston)
+data("BostonHousing")
 
 ## Preview
-head(Boston)
+head(BostonHousing)
 
 ## View the structure
-str(Boston)
+str(BostonHousing)
 
 
 # ---------------------------------------
 
 
-# Prepare the data for regression task
-
-## Create a new dataset
-bt <- Boston
-
-## Convert `chas` to factor
-bt$chas <- factor(bt$chas,
-                  levels = c(1, 0),
-                  labels = c("tract bounds river", "otherwise"))
-
-## Check the result
-str(bt)
-
-
-# ---------------------------------------
-
-
-## Pre-processing
-
-## Remove near-zero variance (NZV) variables
-nzv_vars <- nearZeroVar(bt[, -14])
-
-## Print nzv_vars
-if (length(nvz_vars) > 0) {
-  
-  ## Remove NZV variables
-  bt <- bt[, !(names(bt) %in% nvz_vars)]
-  
-  ## Print a confirmation message
-  print("Near-zero variance variables removed.")
-  
-} else {
-  
-  ## Print a help text
-  print("No near-zero variance variables found.")
-  
-}
-
-## Check the results
-str(bt)
-
-
-# ---------------------------------------
-
-
-## Split the data
+# Split the data
 
 ## Set seed for reproducibility
-set.seed(2025)
+set.seed(888)
 
-## Get training rows
-train_index <- createDataPartition(bt$medv,
-                                   p = 0.8,
+## Get train index
+train_index <- createDataPartition(BostonHousing$medv,
+                                   
+                                   ## Set aside 70% for training set
+                                   p = 0.7,
+                                   
+                                   ## Return as matrix
                                    list = FALSE)
 
-## Create a training set
-bt_train <- bt[train_index, ]
+## Create training set
+bt_train <- BostonHousing[train_index, ]
 
-## Create a test set
-bt_test  <- bt[-train_index, ]
+## Create test set
+bt_test <- BostonHousing[-train_index, ]
 
 
 # ---------------------------------------
 
 
-## Train a random forest
+# Preprocessing
 
-## Set seed for reproducibility
-set.seed(2025)
+## Learn preprocessing on training set
+ppc <- preProcess(bt_train[, -14],
+                  
+                  ## Centre and scaling
+                  method = c("center",
+                             "scale"))
+
+## Apply preprocessing to training set
+bt_train_processed <- predict(ppc,
+                              bt_train[, -14])
+
+## Combine the preprocessed training set with outcome
+bt_train_processed <- cbind(bt_train_processed,
+                            medv = bt_train$medv)
+
+## Apply preprocessing to test set
+bt_test_processed <- predict(ppc,
+                             bt_test[, -14])
+
+## Combine the preprocessed test set with outcome
+bt_test_processed <- cbind(bt_test_processed,
+                            medv = bt_test$medv)
+
+
+# ---------------------------------------
+
+
+# Train the model
+
+## Define training control
+trc <- trainControl(method= "cv",
+                    number = 5)
 
 ## Train
-dt_model <- train(medv ~ .,
-                  data = bt_train,
-                  method = "ranger",
-                  trControl = trainControl(method = "cv",
-                                           number = 5,
-                                           verboseIter = TRUE))
+knn_model <- train(medv ~ .,
+                   
+                   ## Use training set
+                   data = bt_train_processed,
+                   
+                   ## Use knn engine
+                   method = "knn",
+                   
+                   ## Specify training control
+                   trControl = trc,
+                   
+                   ## Set tune length to 10
+                   tuneLength = 10)
+
+## Print the model
+knn_model
 
 
 # ---------------------------------------
 
 
-## Evaluate the model
+# Evaluate the model
 
-## Predict on test data
-predictions <- predict(dt_model,
-                       newdata = bt_test)
+## Make predictions
+predictions <- predict(knn_model,
+                       newdata = bt_test_processed)
 
-## Evaluate performance
-init_mod_results <- postResample(pred = predictions,
-                                 obs = bt_test$medv)
+## Calculate MAE
+mae <- MAE(predictions,
+           bt_test_processed$medv)
+
+## Calculate RMSE
+rmse <- RMSE(predictions,
+             bt_test_processed$medv)
+
+## Calculate R squared
+r2 <- R2(predictions,
+         bt_test_processed$medv)
+
+## Combine the results
+results <- data.frame(MAE = round(mae, 2),
+                      RMSE = round(rmse, 2),
+                      R_Squared = round(r2, 2))
 
 ## Print the results
-init_mod_results
-
-
-# ---------------------------------------
-
-
-## Tune the model
-
-## Define tune grid
-tune_grid <- expand.grid(mtry = c(4, 6, 8, 10),
-                         min.node.size = c(3, 5, 7),
-                         splitrule = c("variance", "extratrees"))
-
-## Set seed for reproducibility
-set.seed(2025)
-
-## Train
-dt_model_tuned <- train(medv ~ .,
-                        data = bt_train,
-                        method = "ranger",
-                        trControl = trainControl(method = "cv",
-                                                 number = 5,
-                                                 verboseIter = TRUE),
-                        tuneGrid = tune_grid,
-                        numtree = 1000)
-
-
-# ---------------------------------------
-
-
-## Evaluate the tuned model
-
-## Predict on test data
-pred_new <- predict(dt_model_tuned,
-                    newdata = bt_test)
-
-## Evaluate performance
-tuned_mod_results <- postResample(pred = pred_new,
-                                  obs = bt_test$medv)
-
-## Print the results
-tuned_mod_results
-
-## Compare the models
-bind_rows(initial = init_mod_results,
-          tuned = tuned_mod_results,
-          .id = "Model")
+results
