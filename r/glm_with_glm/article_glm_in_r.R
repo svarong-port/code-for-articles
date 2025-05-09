1b1374f526bc4796dae3a88d9401b33369d1138e
Shinin Varongchayakul <shivarong@gmail.com>
2025-05-09 21:11:44 +0700

    Reduce code to summary() and coef()

--- r/glm_with_glm/article_glm_in_r.R
+++ r/glm_with_glm/article_glm_in_r.R
@@ -63,7 +63,7 @@ print(coffee_shop)
 
 # Linear regression: Predicting sales
 
-## Fit the model
+## Create a regression model with glm()
 linear_reg <- glm(sales ~ temp + promo + weekend,
                   data = coffee_shop,
                   family = gaussian)
@@ -71,40 +71,13 @@ linear_reg <- glm(sales ~ temp + promo + weekend,
 ## Get model summary
 summary(linear_reg)
 
-## Make predictions
-linear_reg_preds <- predict(linear_reg,
-                            type = "response")
-
-## Compare predicted vs actual
-linear_reg_results <- data.frame(
-  predicted = round(linear_reg_preds, 2),
-  actual = coffee_shop$sales
-  )
-
-## Print the results
-linear_reg_results
-
-## Plot predicted vs actual
-plot(linear_reg_results$predicted,
-     linear_reg_results$actual,
-     main = "Linear Regression: Predicted vs Actual Sales",
-     xlab = "Predicted Sales",
-     ylab = "Actual Sales",
-     pch = 16,
-     col = "darkgreen")
-abline(0,
-       1,
-       col = "red",
-       lwd = 2,
-       lty = 2)
-
 
 # ----------------------------------------------------------
 
 
 # Logistic regression: Predict probability of sold-out
 
-## Fit the model
+## Create a logistic regression model
 log_reg <- glm(sold_out ~ temp + promo + weekend,
                data = coffee_shop,
                family = binomial)
@@ -115,33 +88,13 @@ summary(log_reg)
 ## Transform coefficient
 exp(coef(log_reg))
 
-## Get predictive probabilities
-log_reg_probs <- predict(log_reg,
-                         type = "response")
-
-## Compare predicted vs actual
-log_reg_results <- data.frame(
-  probability = round(log_reg_probs, 2),
-  predicted = ifelse(log_reg_probs > 0.5,
-                     "Yes",
-                     "No"),
-  actual = coffee_shop$sold_out
-)
-
-## Print the results
-log_reg_results
-
-## Create a confusion matrix
-table(predicted = log_reg_results$predicted,
-      actual = log_reg_results$actual)
-
 
 # ----------------------------------------------------------
 
 
 # Poisson regression: Predicting the number of customers per day
 
-## Fit the model
+## Create a poisson regression model
 poisson_reg <- glm(customers ~ temp + promo + weekend,
                    data = coffee_shop,
                    family = poisson)
@@ -151,26 +104,3 @@ summary(poisson_reg)
 
 ## Transform the coefficients
 exp(coef(poisson_reg))
-
-## Make predictions
-poisson_reg_preds <- predict(poisson_reg,
-                             type = "response")
-
-## Compare predicted vs actual
-poisson_reg_results <- data.frame(
-  predicted = round(poisson_reg_preds, 0),
-  actual = coffee_shop$customers
-  )
-
-## Print the results
-poisson_reg_results
-
-## Plot predicted vs actual
-plot(poisson_reg_results$predicted,
-     poisson_reg_results$actual,
-     main = "Predicted vs Actual Customers (Poisson)",
-     xlab = "Predicted",
-     ylab = "Actual")
-abline(0,
-       1,
-       col = "blue")
