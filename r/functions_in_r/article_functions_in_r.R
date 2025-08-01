f68056690894175b94b21b401a99b54cb3ec36e4
Shinin Varongchayakul <shivarong@gmail.com>
2025-08-01 22:03:55 +0700

    Initial commit

---
+++ r/functions_in_r/article_functions_in_r.R
@@ -0,0 +1,68 @@
+# Code for Function in R Article
+
+# Without function
+1 + 2 + 3 + 4 + 5
+
+# With function
+sum(1:5)
+
+
+# Randomly pick 3 numbers between 1 to 10
+sample(1:10, 3)
+
+# Positional arguments
+sample(1:10, 3)
+
+# Keyword arguments
+sample(x = 1:10, size = 3)
+
+
+# Read about mean()
+?mean
+
+# Learn about arguments for mean()
+args(mean)
+
+
+# Data frame of my friends
+friends <- data.frame(
+  name = c("Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "Heidi"),
+  age = c(28, 32, 25, 30, 29, 35, 27, 31)
+)
+
+# friends data frame:
+#       name age
+#  1   Alice  28
+#  2     Bob  32
+#  3 Charlie  25
+#  4   David  30
+#  5     Eve  29
+#  6   Frank  35
+#  7   Grace  27
+#  8   Heidi  31
+
+# Sample 3 of my frends
+sample_n(friends, 3)
+
+# Install package
+install.packages("dplyr")
+
+# Load package
+library(dplyr)
+
+# Sample 3 of my frends
+sample_n(friends, 3)
+
+
+# Custome function to find circle area
+circle_area <- function(radius) {
+  
+  # Calculate area
+  area <- pi * (radius^2)
+  
+  # Return area
+  print(area)
+}
+
+# Calculate circle area where radius is 14
+circle_area(14)
