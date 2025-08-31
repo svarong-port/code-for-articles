650d6bc1ffda38280fcc8a4b48f4097efd299713
Shinin Varongchayakul <shivarong@gmail.com>
2025-08-31 17:28:31 +0700

    Initial commit

---
+++ r/sqldf/article_sqldf.R
@@ -0,0 +1,81 @@
+# Code for sqldf Article
+
+# Install packages
+install.packages("sqldf")
+
+# Load packages
+library(sqldf)
+
+
+# Load the dataset
+data(Cars93)
+
+# Preview the dataset
+head(Cars93)
+
+
+
+# Example 1. SELECT
+
+# Set query
+select_query <- "
+SELECT
+  Manufacturer,
+  Model,
+  `Min.Price`,
+  `Max.Price`
+FROM
+  Cars93
+"
+
+# Select from df
+select_result <- sqldf(select_query)
+
+# View the result
+select_result
+
+
+
+# Example 2. WHERE
+
+# Set query
+where_result <- "
+SELECT
+  Manufacturer,
+  Model,
+  `Min.Price`,
+  `Max.Price`
+FROM
+  Cars93
+WHERE
+  Origin = 'non-USA';
+"
+
+# Filter df
+where_result <- sqldf(where_result)
+
+# View the result
+where_result
+
+
+
+# Example 3. Aggregate data
+
+# Set query
+aggregate_query <- "
+SELECT
+  Manufacturer,
+  AVG(Price) AS Avg_Price
+FROM
+  Cars93
+GROUP BY
+  Manufacturer
+ORDER BY
+  Avg_Price DESC;
+"
+
+# Aggregate df
+aggregate_result <- sqldf(aggregate_query)
+
+# View the result
+aggregate_result
