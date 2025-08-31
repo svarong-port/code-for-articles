5e09b5c1e594d4dac6d96fb46ac56e0d571ae15b
Shinin Varongchayakul <shivarong@gmail.com>
2025-08-31 19:20:26 +0700

    Initial commit

---
+++ r/intro_to_dbplyr/article_intro_to_dbplyr.R
@@ -0,0 +1,60 @@
+# Code for dbplyr Article
+
+# Install packages
+install.packages("DBI")
+install.packages("RSQLite")
+install.packages("dplyr")
+install.packages("dbplyr")
+
+# Load packages
+library(DBI)
+library(RSQLite)
+library(dplyr)
+library(dbplyr)
+
+
+# Connect to database
+con <- dbConnect(RSQLite::SQLite(), "chinook.sqlite")
+
+# View all tables
+dbListTables(con)
+
+
+# Create lazy tibble
+tracks <- tbl(con, "Track")
+
+# View tibble
+tracks
+
+
+# Working with database table
+album_info <- tracks |>
+  
+  # Group by album
+  group_by(AlbumId) |>
+  
+  # Summarise
+  summarise(
+    
+    # Number of tracks
+    tracks = n(),
+    
+    # Average duration
+    mean_millisec = mean(Milliseconds, na.rm = TRUE),
+    
+    # Total size
+    total_bytes = sum(Bytes)
+  ) |>
+  
+  # Sort by duration
+  arrange(desc(mean_millisec))
+
+# Show query
+show_query(album_info)
+
+# Get result
+collect(album_info)
+
+
+# Disconnect from database
+dbDisconnect(con)
